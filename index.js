845f4d7c0348e1c30ac9f47ab3d1e030ac465caa
Jose Salazar <solisjose466@gmail.com>
2021-05-28 17:27:41 -0700

    ui work

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-// var myObject = [
-//     {
-//         top: 23,
-//         bottom: 33,
-//         left: 69,
-//         right: 42
-//     }
-//     //the rest of cells...
-// ];
-
 var editor = CodeMirror.fromTextArea(document.getElementById('editor'),{
     mode: "javascript",
     theme: "default",
