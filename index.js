fe2bc18fa004cfd1869aec206e0c21298f480d60
Jose Salazar <solisjose466@gmail.com>
2021-05-28 19:42:09 -0700

    js ui

--- index.js
+++ index.js
@@ -1,39 +1,19 @@
+var topPanelBtns = document.getElementsByClassName('btnJS');
+var activeClass = 'btn btn-primary active btnJS';
+var inactiveClass = 'btn btn-outline-primary btnJS';
+
+for(var i = 0; i < topPanelBtns.length; i++){
+    topPanelBtns[i].addEventListener('click', toggleBtn);
+}
+
 var editor = CodeMirror.fromTextArea(document.getElementById('editor'),{
     mode: "javascript",
     theme: "default",
     lineNumbers: true,
     readOnly: true
 });
-var activeClass = 'btn btn-primary active btnJS';
-var inactiveClass = 'btn btn-outline-primary btnJS';
 
-function PointsAction(){
-    if(!sessionObject.Points){
-        rectanglesBtn.className = inactiveClass;
-        pointsBtn.className = activeClass;
-
-        sessionObject.Points = true;
-        //change to TBLR
-    }
-}
-
-function RectanglesAction(){
-    if(sessionObject.Points){
-        rectanglesBtn.className = activeClass;
-        pointsBtn.className = inactiveClass;
-
-        sessionObject.Points = false;
-        //change to TBLRHW
-    }
-}
-
-var topPanelBtns = Array.from(document.getElementsByClassName('btnJS'));
-
-for(var i = 0; i < topPanelBtns.length; i++){
-    topPanelBtns[i].addEventListener('click', BtnToggle);
-}
-
-function BtnToggle(){
+function toggleBtn(){
     if(this.id == 'Points'){
         document.getElementById('Rectangles').className = inactiveClass;
         this.className = activeClass;
@@ -58,4 +38,8 @@ function BtnToggle(){
         document.getElementById('Min').className = inactiveClass;
         this.className = activeClass;
     }
+}
+
+function showOptions(){
+    
 }
