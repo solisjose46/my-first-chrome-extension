f07936d2bfb3de17b9728ab0638250ffaf528073
Jose Salazar <solisjose466@gmail.com>
2021-05-29 18:55:56 -0700

    ui and functionality

---
+++ testing/index.js
@@ -0,0 +1,56 @@
+//make html elements draggable
+dragElement(document.getElementById('top-panel'));
+dragElement(document.getElementById('side-panel'));
+
+//since points is the default
+//optionsHTML.height = document.getElementById('Height');
+//optionsHTML.width = document.getElementById('Width');
+//document.getElementById('Height').remove();
+//document.getElementById('Width').remove();
+
+for(var i=0; i<buttons.length; i++){
+    buttons[i].addEventListener('click', clicked);
+}
+
+function clicked(){
+    if(this.id == 'Points' || this.id == 'Rectangles'){
+        changeOptions(this.id);
+    }
+    else if(this.id == 'Primary'){
+        if(this.className == activeClass){
+            this.className = inactiveClass;
+            document.getElementById('Secondary').className = activeClass;
+        }
+        else{
+            this.className = activeClass;
+            document.getElementById('Secondary').className = inactiveClass;
+        }
+    }
+    else if(this.id == 'Secondary'){
+        if(this.className == activeClass){
+            this.className = inactiveClass;
+            document.getElementById('Primary').className = activeClass;
+        }
+        else{
+            this.className = activeClass;
+            document.getElementById('Primary').className = inactiveClass;
+        }
+    }
+    else if(this.id == 'Min' || this.id == 'Max'){
+        SidePanelCollapse(this.id);
+    }
+    else if(this.id == 'Plus' || this.id == 'Minus'){
+        TopPanelCollapse(this.id);
+    }
+    else if(this.id == 'Top' || this.id == 'Bottom' || this.id == 'Left' || this.id == 'Right' || this.id == 'Height' || this.id == 'Width'){
+        if(this.className == activeClass){
+            this.className = inactiveClass;
+        }
+        else{
+            this.className = activeClass;
+        }
+    }
+    else if(this.id == 'make-table'){
+        MakeTable();
+    }
+}
---
+++ testing/top-panel.js
@@ -0,0 +1,67 @@
+function TopPanelCollapse(collapse){
+    document.getElementById(collapse).className = activeClass;
+    if(document.getElementById('insert-top-panel').childElementCount <= 2 && collapse == 'Plus'){
+        document.getElementById('Minus').className = inactiveClass;
+
+        document.getElementById('insert-top-panel').appendChild(topPanelHtml.group1);
+        document.getElementById('insert-top-panel').appendChild(topPanelHtml.group2);
+        document.getElementById('insert-top-panel').appendChild(topPanelHtml.group3);
+    }
+    else if(document.getElementById('insert-top-panel').childElementCount >= 5 && collapse == 'Minus'){
+        document.getElementById('Plus').className = inactiveClass;
+
+        topPanelHtml.group1 = document.getElementById('group1');
+        topPanelHtml.group2 = document.getElementById('group2');
+        topPanelHtml.group3 = document.getElementById('group3');
+
+        document.getElementById('group1').remove();
+        document.getElementById('group2').remove();
+        document.getElementById('group3').remove();
+    }
+}
+function SidePanelCollapse(collapse){
+    document.getElementById(collapse).className = activeClass;
+    if(collapse == 'Min' && panelHtml.collapsed == false){
+        var sidePanel = document.getElementById('side-panel');
+        panelHtml.side = sidePanel;
+        sidePanel.remove();
+        panelHtml.collapsed = true;
+        document.getElementById('Max').className = inactiveClass;
+    }
+    else if(collapse == 'Max' && panelHtml.collapsed == true){
+        document.body.appendChild(panelHtml.side);
+        panelHtml.collapsed = false;
+        document.getElementById('Min').className = inactiveClass;
+    }
+}
+function changeOptions(option){
+    document.getElementById(option).className = activeClass;
+    var table = document.getElementById('table-options');
+    if(option == 'Points' && table.childElementCount == 6){
+        document.getElementById('Rectangles').className = inactiveClass;
+        var height = document.getElementById('Height');
+        var width = document.getElementById('Width');
+        optionsHTML.height = height;
+        optionsHTML.width = width;
+        height.remove();
+        width.remove();
+    }
+    else if(option == 'Rectangles' && table.childElementCount == 4){
+        document.getElementById('Points').className = inactiveClass;
+        optionsHTML.height.addEventListener('click', clicked);
+        optionsHTML.width.addEventListener('click', clicked);
+        table.appendChild(optionsHTML.height);
+        table.appendChild(optionsHTML.width);
+
+    }
+}
+function changeTheme(theme){
+    document.getElementById(theme).className = activeClass;
+    if(theme == 'Primary'){
+        document.getElementById('Secondary').className = inactiveClass;
+        //
+    }
+    else{
+        document.getElementById('Primary').className = inactiveClass;
+    }
+}
